a1c239ce2e79cffe6aae5da94ad1f2d4541eefd4
Apurva Anand <apurvanand29@gmail.com>
2025-06-02 16:24:03 -0500

    login test 2 on OHRM

--- PracticeTest1/tests/login.spec.js
+++ PracticeTest1/tests/login.spec.js
@@ -10,6 +10,16 @@ test('Login Test', async ({ page }) => {
     await page.waitForSelector('text=Sign in', { timeout: 4000 })
     await expect(page.locator('text=Sign in')).toHaveCount(1)
     await page.getByRole('link', { name: 'Sign in' }).click()
+})
 
+test.only('Login demo test 2', async ({ page }) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.pause()
+    await page.locator('[placeholder="Username"]').fill('Admin')
+    await page.locator('[placeholder="Password"]').fill('admin123')
+    await page.locator('button:has-text("Login")').click()
+    await page.getByText('manda user').click()
+    await page.getByText('Logout').click();
 
 })
