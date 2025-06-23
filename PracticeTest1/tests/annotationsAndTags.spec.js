240a1985e54564533544f7c5407be10d68b1ea01
Apurva Anand <apurvanand29@gmail.com>
2025-06-23 12:38:15 -0500

    documentation for tags

--- PracticeTest1/tests/annotationsAndTags.spec.js
+++ PracticeTest1/tests/annotationsAndTags.spec.js
@@ -12,6 +12,7 @@ test.fixme('Test 3', async ({ page }) => {
     // This test is marked as "fixme" and will be reported as such in the test results.
 })
 
+// to run particular tag, use command: npx playwright test annotationsAndTags --project chromium --grep "@smoke"
 test('Test 4 @smoke', async ({ page }) => {
     test.slow();
 })
