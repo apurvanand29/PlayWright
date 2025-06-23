45b03523a4f916166278d1ef98ac1f150828c1da
Apurva Anand <apurvanand29@gmail.com>
2025-06-22 21:10:39 -0500

    added slowMo and video capture attributes

--- PracticeTest1/tests/slowMotion_VideoRecording.spec.js
+++ PracticeTest1/tests/slowMotion_VideoRecording.spec.js
@@ -1,8 +1,8 @@
 import { test, expect, chromium } from '@playwright/test'
 
 test('slow motion video recording', async () => {
-    const browser = await chromium.launch();
-    const context = await browser.newContext();
+    const browser = await chromium.launch({ slowMo: 500, headless: false });
+    const context = await browser.newContext({ recordVideo: { dir: 'videos/', size: { width: 800, height: 600 } } });
     const page = await context.newPage();
     await page.goto('');
     await context.close();
