9d8ec1ac4b9f5042d26e30f7186df0a2254f9759
Apurva Anand <apurvanand29@gmail.com>
2025-06-22 23:19:27 -0500

    learning descrip group

--- PracticeTest1/tests/hooksAndGroups.spec.js
+++ PracticeTest1/tests/hooksAndGroups.spec.js
@@ -1,21 +1,23 @@
 import { test, expect } from '@playwright/test'
 
-test.beforeEach(async ({ page }) => {
+test.describe('Login and add to cart', () => {
+    test.beforeEach(async ({ page }) => {
 
-    await page.goto('https:www.saucedemo.com')
-    await page.locator('[data-test="username"]').click();
-    await page.locator('[data-test="username"]').fill('standard_user');
-    await page.locator('[data-test="password"]').click();
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click();
-})
+        await page.goto('https:www.saucedemo.com')
+        await page.locator('[data-test="username"]').click();
+        await page.locator('[data-test="username"]').fill('standard_user');
+        await page.locator('[data-test="password"]').click();
+        await page.locator('[data-test="password"]').fill('secret_sauce');
+        await page.locator('[data-test="login-button"]').click();
+    })
 
 
-test('home page', async ({ page }) => {
+    test('home page', async ({ page }) => {
 
-    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-    await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
-    await page.locator('[data-test="item-1-title-link"]').click();
-    await page.locator('[data-test="add-to-cart"]').click();
-    await page.close();
+        await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+        await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+        await page.locator('[data-test="item-1-title-link"]').click();
+        await page.locator('[data-test="add-to-cart"]').click();
+        await page.close();
+    })
 })
