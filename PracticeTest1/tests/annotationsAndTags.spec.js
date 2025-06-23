5cb6ce86dc5765c4f3568b768f5db0f618b3a6c9
Apurva Anand <apurvanand29@gmail.com>
2025-06-23 12:31:23 -0500

    test3: fixme

--- PracticeTest1/tests/annotationsAndTags.spec.js
+++ PracticeTest1/tests/annotationsAndTags.spec.js
@@ -7,3 +7,10 @@ test.skip('Test 1', async ({ page }) => {
 test('not ready yet', async ({ page }) => {
     test.fail();
 })
+
+test.fixme('Test 3', async ({ page }) => {
+    // This test is marked as "fixme" and will be reported as such in the test results.
+})
+
+
+test.slow('Test 4', async ({ page }) => {
