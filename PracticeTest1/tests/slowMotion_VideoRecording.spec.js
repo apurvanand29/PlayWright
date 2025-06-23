b74f4ffd13554a44a963ab6002acc6f9366519e5
Apurva Anand <apurvanand29@gmail.com>
2025-06-22 21:20:12 -0500

    video recorded

--- PracticeTest1/tests/slowMotion_VideoRecording.spec.js
+++ PracticeTest1/tests/slowMotion_VideoRecording.spec.js
@@ -4,6 +4,10 @@ test('slow motion video recording', async () => {
     const browser = await chromium.launch({ slowMo: 500, headless: false });
     const context = await browser.newContext({ recordVideo: { dir: 'videos/', size: { width: 800, height: 600 } } });
     const page = await context.newPage();
-    await page.goto('');
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.pause()
+    await page.locator('[placeholder="Username"]').fill('Admin')
+    await page.locator('[placeholder="Password"]').fill('admin123')
+    await page.locator('button:has-text("Login")').click()
     await context.close();
 })
