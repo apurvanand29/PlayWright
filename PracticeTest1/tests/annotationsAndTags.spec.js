bc0bcb46d53ff22434cebc712644c42775a1ec09
Apurva Anand <apurvanand29@gmail.com>
2025-06-23 12:54:43 -0500

    Doc for annotations and tags

--- PracticeTest1/tests/annotationsAndTags.spec.js
+++ PracticeTest1/tests/annotationsAndTags.spec.js
@@ -1,5 +1,8 @@
 import { test } from '@playwright/test'
 
+// Annotations: skip, fixme, fail, slow, skip conditionally, only, etc.
+// Tags: smoke, regression, etc.
+// You can use annotations and tags to categorize and manage your tests effectively.
 test.skip('Test 1', async ({ page }) => {
 
 })
@@ -13,6 +16,7 @@ test.fixme('Test 3', async ({ page }) => {
 })
 
 // to run particular tag, use command: npx playwright test annotationsAndTags --project chromium --grep "@smoke"
+// if you want to skip a tag, you can use the --grep-invert command: npx playwright test annotationsAndTags --project chromium --grep-invert "@smoke"
 test('Test 4 @smoke', async ({ page }) => {
     // if test is marked as slow, it will be reported as such in the test results and the timeout for the test will be tripled.
     test.slow();
