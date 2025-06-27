3afab9ebf1fe857efc312dcfac6dfed68208b550
Apurva Anand <apurvanand29@gmail.com>
2025-06-27 16:16:48 -0500

    updated locator

--- PlaywrightPOM/pages/login.js
+++ PlaywrightPOM/pages/login.js
@@ -3,8 +3,12 @@ exports.LoginPage = class LoginPage {
     constructor(page) {
         this.page = page;
         this.usernameInput = page.getByLabel('Username')
-        this.passwordInput = page.getByRole('Password')
+        this.passwordInput = page.getByLabel('Password')
         this.loginButton = page.getByRole('button', { name: ' Login' })
+    }
+
+    async gotoLoginPage() {
+        await this.page.goto('https://the-internet.herokuapp.com/login');
     }
 
     // created onemethod for login instead of atomic actions
--- PlaywrightPOM/tests/login.spec.js
+++ PlaywrightPOM/tests/login.spec.js
@@ -6,10 +6,14 @@ import { LoginPage } from '../pages/login';
 test('test', async ({ page }) => {
     const Login = new LoginPage(page);
 
-    await page.goto('https://the-internet.herokuapp.com/login');
+    await Login.gotoLoginPage()
+    await Login.login('tomsmith', 'SuperSecretPassword!');
+
+    /**await page.goto('https://the-internet.herokuapp.com/login');
     await page.getByRole('textbox', { name: 'Username' }).click();
     await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
     await page.getByRole('textbox', { name: 'Password' }).click();
     await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
     await page.getByRole('button', { name: ' Login' }).click();
+**/
 });
